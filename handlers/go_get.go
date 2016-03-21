1e0d3015a1733f4305d69b81d663135fc0165789
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-21 16:47:30 -0700

    starting on the go get handler

--- handlers/go_get.go
+++ handlers/go_get.go
@@ -1,18 +1,42 @@
 package handlers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+const (
+	goGetRespTplStr = `<html>
+<head>
+	<meta name="go-import" content="{{.Prefix}} {{.VCS}} {{.RepoRoot}}></meta>
+</head>
+<body>
+	{{.GoGetCmd}}
+</body>
+</html>`
+)
+
+var (
+	goGetRespTpl = template.Must(template.New("GoGetTpl").Parse(goGetRespTplStr))
+)
+
+type goGetTplData struct {
+	Prefix   string
+	VCS      string
+	RepoRoot string
+}
+
 // GoGet is Handler implementation to handle the endpoint that "go get" makes requests to.
 // for example, it is able to handle "https://goproxserver.com/github.com/my/package?go-get=1"
 type GoGet struct {
+	VCS     string
+	GitHost string
 }
 
 func (g *GoGet) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
+	tplData := goGetTplData{Prefix: prefix, VCS: g.VCS, RepoRoot: g.RepoRoot}
 }
 
 // Register is the Handler interface implementation
