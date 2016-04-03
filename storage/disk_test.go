da621951ae4d58de5254e96f2eb5bfba22795bfb
Aaron Schlesinger <arschles@gmail.com>
2016-04-03 16:08:03 -0700

    writing more tests
    and factoring out the test data related functionality into a new
    utility package

--- storage/disk_test.go
+++ storage/disk_test.go
@@ -1,9 +1,67 @@
 package storage
 
 import (
+	"archive/tar"
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/arschles/assert"
+	"github.com/arschles/goprox/tests"
 )
 
 func TestUntarToDisk(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "goproxtest")
+	assert.NoErr(t, err)
+	defer func() {
+		if rerr := os.RemoveAll(tmpDir); rerr != nil {
+			log.Printf("Error removing temp dir %s (%s)", tmpDir, rerr)
+		}
+	}()
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+	testDataDir, err := tests.DataDir()
+	assert.NoErr(t, err)
+	files := tests.ExpectedDataSlice()
+	for _, file := range files {
+		absFile := filepath.Join(testDataDir, file)
+		fBytes, err := ioutil.ReadFile(absFile)
+		if err != nil {
+			t.Errorf("reading file %s (%s)", file, err)
+			continue
+		}
+		hdr := &tar.Header{
+			Name: file,
+			Mode: int64(os.ModePerm),
+			Size: int64(len(fBytes)),
+		}
+		if err := tw.WriteHeader(hdr); err != nil {
+			t.Errorf("writing tar header for %s", file)
+			continue
+		}
+		if _, err := tw.Write(fBytes); err != nil {
+			t.Errorf("writing bytes for %s (%s)", file, err)
+			continue
+		}
+	}
+	assert.NoErr(t, UntarToDisk(buf, tmpDir))
+	pathSet := tests.ExpectedDataSet()
+	numFound := 0
+	fwErr := filepath.Walk(tmpDir, func(path string, info os.FileInfo, err error) error {
+		if _, ok := pathSet[path]; !ok {
+			t.Errorf("unexpected path found: %s", path)
+			return nil
+		}
+		numFound++
+		return nil
+	})
+	assert.NoErr(t, fwErr)
+	if numFound != len(pathSet) {
+		t.Errorf("found %d paths, expected %d", numFound, len(pathSet))
+		return
+	}
 
 }
--- storage/upload_test.go
+++ storage/upload_test.go
@@ -2,55 +2,19 @@ package storage
 
 import (
 	"archive/tar"
-	"fmt"
 	"io"
-	"log"
-	"os"
-	"path/filepath"
 	"testing"
 
 	"github.com/arschles/assert"
+	"github.com/arschles/goprox/tests"
 )
-
-var (
-	goPath           string
-	testDataDirFiles = []string{
-		"dir1/file1",
-		"dir2/dir1/dir1/file1",
-		"dir2/dir1/dir1/dir1/file1",
-		"file1",
-	}
-)
-
-func init() {
-	goPath = os.Getenv("GOPATH")
-	if goPath == "" {
-		log.Fatalf("GOPATH env var not found, cannot continue")
-	}
-}
-
-func testDataDirFilesSet() map[string]struct{} {
-	ret := make(map[string]struct{})
-	for _, file := range testDataDirFiles {
-		ret[file] = struct{}{}
-	}
-	return ret
-}
-
-func getTestDataDir() (string, error) {
-	dir, err := filepath.Abs(fmt.Sprintf("%s/src/github.com/arschles/goprox/testdata", goPath))
-	if err != nil {
-		return "", err
-	}
-	return dir, nil
-}
 
 func TestGetFiles(t *testing.T) {
-	dir, err := getTestDataDir()
+	dir, err := tests.DataDir()
 	assert.NoErr(t, err)
 	files, err := getFiles(dir)
 	assert.NoErr(t, err)
-	expected := testDataDirFiles
+	expected := tests.ExpectedDataSlice()
 	set := map[string]struct{}{}
 	for _, file := range files {
 		set[file] = struct{}{}
@@ -62,12 +26,12 @@ func TestGetFiles(t *testing.T) {
 }
 
 func TestTarFiles(t *testing.T) {
-	dir, err := getTestDataDir()
+	dir, err := tests.DataDir()
 	assert.NoErr(t, err)
-	rdr, err := tarFiles(dir, testDataDirFiles)
+	rdr, err := tarFiles(dir, tests.ExpectedDataSlice())
 	assert.NoErr(t, err)
 	tr := tar.NewReader(rdr)
-	set := testDataDirFilesSet()
+	set := tests.ExpectedDataSet()
 	numFound := 0
 	for {
 		hdr, err := tr.Next()
@@ -84,8 +48,8 @@ func TestTarFiles(t *testing.T) {
 		}
 		numFound++
 	}
-	if numFound != len(testDataDirFiles) {
-		t.Fatalf("found only %d of %d files in the testdata dir", len(testDataDirFiles), numFound)
+	if numFound != len(set) {
+		t.Fatalf("found only %d of %d files in the testdata dir", len(tests.ExpectedDataSlice()), numFound)
 	}
 
 }
