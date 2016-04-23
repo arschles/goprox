e4ee9ab4d029d5dbcbdfa194d8ea2bfcca1c70f6
Aaron Schlesinger <arschles@gmail.com>
2016-04-23 11:42:03 -0700

    breaking up the go get handler and starting to add tests for the new funcs

--- handlers/go_get.go
+++ handlers/go_get.go
@@ -31,6 +31,26 @@ func getPackage(u *url.URL) string {
 	return u.Path[1:]
 }
 
+func standardPort(scheme string, port int) bool {
+	return (scheme == "http" && port == 80) || (scheme == "https" && port == 443)
+}
+
+func getRepoRoot(gitScheme, gitHost, pkg string, outwardPort int) string {
+	repoRoot := fmt.Sprintf("%s://%s:%d/%s", gitScheme, gitHost, outwardPort, pkg)
+	if standardPort(gitScheme, outwardPort) {
+		repoRoot = fmt.Sprintf("%s://%s/%s", gitScheme, gitHost, pkg)
+	}
+	return repoRoot
+}
+
+func getImportPrefix(gitScheme, webHost, pkg string, outwardPort int) string {
+	importPrefix := fmt.Sprintf("%s:%d/%s", webHost, outwardPort, pkg)
+	if standardPort(gitScheme, outwardPort) {
+		importPrefix = fmt.Sprintf("%s/%s", webHost, pkg)
+	}
+	return importPrefix
+}
+
 func goGet(webHost string, outwardPort int, gitScheme, gitHost string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Query().Get(goGetQueryKey) != "1" {
@@ -38,12 +58,8 @@ func goGet(webHost string, outwardPort int, gitScheme, gitHost string) http.Hand
 			return
 		}
 		pkg := getPackage(r.URL)
-		repoRoot := fmt.Sprintf("%s://%s:%d/%s", gitScheme, gitHost, outwardPort, pkg)
-		importPrefix := fmt.Sprintf("%s:%d/%s", webHost, outwardPort, pkg)
-		if (gitScheme == "http" && outwardPort == 80) || (gitScheme == "https" && outwardPort == 443) {
-			repoRoot = fmt.Sprintf("%s://%s/%s", gitScheme, gitHost, pkg)
-			importPrefix = fmt.Sprintf("%s/%s", webHost, pkg)
-		}
+		repoRoot := getRepoRoot(gitScheme, gitHost, pkg, outwardPort)
+		importPrefix := getImportPrefix(gitScheme, webHost, pkg, outwardPort)
 		data := goGetTplData{
 			ImportPrefix: importPrefix,
 			RepoRoot:     repoRoot,
