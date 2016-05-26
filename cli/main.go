828578779434541eaf26402fa82e92157198fb75
Aaron Schlesinger <arschles@gmail.com>
2016-05-26 10:46:15 -0700

    adding preliminary cli code

---
+++ cli/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/codegangsta/cli"
+	"google.golang.org/grpc"
+)
+
+const (
+	appName = "goprox"
+)
+
+func main() {
+	log.Printf("hello world!")
+	app := cli.NewApp()
+	app.Run(os.Args)
+
+	// conn, err := grpc.Dial(conf.AdminHostString, grpc.WithInsecure())
+	// if err != nil {
+	// 	log.Fatalf("error dialing server (%s)", err)
+	// }
+	// defer conn.Close()
+}
