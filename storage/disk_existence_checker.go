eb4209a0f3b37a5ba9c585e0be05328deb64765f
Aaron Schlesinger <arschles@gmail.com>
2017-03-15 18:35:35 -0700

    fixing bug in existence checker

--- storage/disk_existence_checker.go
+++ storage/disk_existence_checker.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"os"
 	"path/filepath"
+
+	"github.com/arschles/goprox/logs"
 )
 
 // DiskExistenceChecker is an ExistenceChecker that reads from the Gopath given.
@@ -14,10 +16,13 @@ type DiskExistenceChecker struct {
 
 // Exists is the ExistenceChecker implementation
 func (d DiskExistenceChecker) Exists(ctx context.Context, pkgName, version string) (bool, error) {
-	fullPath := filepath.Join("vendor", "src", pkgName)
+	logger := logs.FromContext(ctx)
+	fullPath := filepath.Join(d.Gopath, "src", pkgName)
+	logger.Printf("checking if %s exists", fullPath)
 	_, err := os.Stat(fullPath)
 	if os.IsNotExist(err) {
+		logger.Printf("%s doesn't exist", fullPath)
 		return false, nil
 	}
-	return false, err
+	return true, err
 }
