a863112875b66fb5bfd849f591d80bfb40fa96b2
Aaron Schlesinger <aaron@deis.com>
2017-03-23 14:17:40 -0700

    removing deploy logic for now
    there’s no more deis cluster for now

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,10 +3,4 @@
 # Build and push Docker images to Docker Hub and quay.io.
 #
 
-cd "$(dirname "$0")" || exit 1
-
-docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-make -C .. docker-push
-curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
-./deis login --username=$DEIS_USERNAME --password=$DEIS_PASSWORD deis.arschles.net
-DEIS_BINARY_NAME=./_scripts/deis DEIS_APP_NAME=goprox make -C .. deploy-to-deis
+echo "nothing to deploy right now"
